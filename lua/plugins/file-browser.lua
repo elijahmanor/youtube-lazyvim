f0b46d25deae624cbae2d713924d0eed6065b3ce
Ayush Sehrawat <69469790+AyushSehrawat@users.noreply.github.com>
2023-02-20 10:48:45 +0530

    Update plugins/file-browser.lua for some bug fixes
    The config had some telescope command issues as mentioned by me in https://github.com/nvim-telescope/telescope-file-browser.nvim/issues/235

--- lua/plugins/file-browser.lua
+++ lua/plugins/file-browser.lua
@@ -1,13 +1,18 @@
 return {
-  "nvim-telescope/telescope-file-browser.nvim",
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-file-browser.nvim",
+  },
   keys = {
     {
       "<leader>sB",
-      ":Telescope file_browser path=%:p:h=%:p:h<cr>",
+      ":Telescope file_browser path=%:p:h<cr>",
       desc = "Browse Files",
     },
   },
   config = function()
+    require("telescope").setup({})
     require("telescope").load_extension("file_browser")
   end,
 }
